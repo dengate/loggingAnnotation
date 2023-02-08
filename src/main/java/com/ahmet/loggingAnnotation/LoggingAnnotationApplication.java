985b7e2865585f3c7a5e31a888520c0bf46ba7a4
Ahmet Emin Hergüner <ahmetemin_97@outlook.com>
2023-02-08 16:20:43 +0300

    initial

---
+++ src/main/java/com/ahmet/loggingAnnotation/LoggingAnnotationApplication.java
@@ -0,0 +1,12 @@
+package com.ahmet.loggingAnnotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggingAnnotationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoggingAnnotationApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ahmet/loggingAnnotation/LoggingAnnotationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahmet.loggingAnnotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingAnnotationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
